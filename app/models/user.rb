c71e5d0f38c50cd7652ba72ae67f97ef4bc4a186
Andrew Waller <andrewhwaller@gmail.com>
2019-06-10 04:50:43 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
 
-  def create
-    user = User.find_or_create_by(:uid => auth['uid']) do |user|
-      user.name = auth['info']['name']
-    end
-    session[:user_id] = user.try(:id)
+  def new
   end
 
-  def auth
-    request.env['omniauth.auth']
+  def create
+  end
+
+  def delete
+
   end
 end
